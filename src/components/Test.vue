d47a155efd7b4760d9cc13678f2e8aeeb80d1733
Lucas Calmon <lucas.calmon11@gmail.com>
2019-11-24 15:39:21 +0100

    Router link

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,13 +1,9 @@
 <template>
-<<<<<<< refs/remotes/origin/master
-  <h1>TEST</h1>
-=======
   <div>
     <h1>TEST PAGE</h1>
     <router-link to="/">back</router-link>
   </div>
   
->>>>>>> Added basic routing
 </template>
 
 <script>
