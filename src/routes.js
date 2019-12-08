ac9a6cdda2abe2742b1d0505496897837bd846a4
Lucas Calmon <lucas.calmon11@gmail.com>
2019-12-08 12:54:09 +0100

    Fix routing

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,6 @@ import PokemonGrid from './components/PokemonGrid/PokemonGrid.vue'
 import Test from './components/Test.vue'
 
 export const routes = [
-    { path: '', component: PokemonGrid },
+    { path: '/', component: PokemonGrid },
     { path: '/test/:id', component: Test, props: true },
-    { path: '*', redirect: '/'}
 ];
